84ea4cf3ff209b43ec2eab026f346292af877355
Mohamad Kahwaji <mohamad.17.kahwaji@gmail.com>
2026-09-01 16:06:54 +0300

    chore: remove temporary diagnostic route

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,3 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
 Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
 Route::get('/lang/{locale}', [LocaleController::class, 'switch'])->name('locale.switch');
-
-// TEMP: diagnostic route, remove after debugging storage symlink.
-Route::get('/__diag-storage-x9k2', function () {
-    abort_unless(request()->query('key') === 'mansour-debug-2026', 404);
-
-    $publicStorage = public_path('storage');
-    $publicDir = storage_path('app/public');
-
-    $linkResult = null;
-    $linkError = null;
-
-    if (request()->query('link') === '1') {
-        try {
-            \Illuminate\Support\Facades\Artisan::call('storage:link', ['--force' => true]);
-            $linkResult = \Illuminate\Support\Facades\Artisan::output();
-        } catch (\Throwable $e) {
-            $linkError = $e->getMessage();
-        }
-    }
-
-    return response()->json([
-        'public_storage_is_link' => is_link($publicStorage),
-        'public_storage_readlink' => is_link($publicStorage) ? readlink($publicStorage) : null,
-        'public_storage_exists' => file_exists($publicStorage),
-        'public_dir_exists' => is_dir($publicDir),
-        'public_dir_contents' => is_dir($publicDir) ? glob($publicDir . '/*') : null,
-        'site_dir_contents' => is_dir($publicDir . '/site') ? glob($publicDir . '/site/*') : null,
-        'target_file_exists' => file_exists($publicDir . '/site/01M1EDMHZ346DQDZQX7GMB6CXT.jpg'),
-        'link_result' => $linkResult,
-        'link_error' => $linkError,
-        'public_path_is_writable' => is_writable(public_path()),
-        'php_user' => function_exists('posix_getpwuid') ? posix_getpwuid(posix_geteuid())['name'] : get_current_user(),
-    ]);
-});
